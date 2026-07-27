e0b7c6b3f21c1bfafb77666bd013f4bae82146ed
seocopy70 <seongkyos@gmail.com>
2026-07-27 23:57:48 +0900

    config.js 추가

---
+++ config.js
@@ -0,0 +1,6 @@
+// Supabase 설정 정보
+const SUPABASE_URL = 'https://mtffgykhsxumdcxpdxwc.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10ZmZneWtoc3h1bWRjeHBkeHdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNDY3OTcsImV4cCI6MjEwMDcyMjc5N30.BuGXiKfMnBJJNjq4I3CVVzhnmzq_EB5sJQk-G3CKF08';
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabase = supabaseClient;
